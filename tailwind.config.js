6b06e514333581fc21d07f0180c0775a6018f5a8
Abbas Moharami <abbasmdev@gmail.com>
2022-10-20 21:48:08 +0330

    chore(theme): add and config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,32 @@ module.exports = {
     "./src/domains/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        abNavy: {
+          dark: "#020c1b",
+          light: "#112240",
+          DEFAULT: "#0a192f",
+          lightest: "#233554",
+
+        },
+        abSlate: {
+          DEFAULT: "#8892b0",
+          dark: "#495670",
+          light: "#a8b2d1",
+          lightest: "#ccd6f6",
+
+        },
+        abWhite: "#e6f1ff",
+        abGreen: {
+          DEFAULT: "#64ffda",
+          tint: "rgba(100,255,218,0.1)"
+        },
+        abPink: "#f57dff",
+        abBlue: "#57cbff"
+
+      }
+    },
   },
   plugins: [],
 }
